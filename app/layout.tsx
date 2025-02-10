6d7533a9ec39eaeeae7623653d270f0ead3c3d47
Krish Goenka <goenkakrish02@gmail.com>
2025-02-10 22:50:02 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "TechXtreme",
+  title: "Aritra Bhaiya Shaadi",
   description: "Two Days. Endless Possibilities",
 };
 
